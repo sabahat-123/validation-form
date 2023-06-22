f5633245dd080c889c52504877078194df3c0db6
sabahat-123 <sabahat.parveen@imtspl.net>
2023-06-22 22:17:10 +0530

    update all

---
+++ form/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import ValidationForm from './ValidationForm';
+
+function App() {
+  return (
+    <div className="App">
+
+     <ValidationForm/>
+    </div>
+  );
+}
+
+export default App;
